4671b7d8e6ec08866e4796f00e9d5c429abbb462
vsaeide <vsaeide7798@gmail.com>
2021-10-18 11:54:56 +0330

    square function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 
+
 def add(x, y):
     return x + y
 
@@ -22,8 +23,14 @@ def divide(x, y):
 def power(x, y):
     return x ** y
 
+
 def log(x, y):
-        return math.log(x, y)
+    return math.log(x, y)
+
+
+def square(x):
+    return math.sqrt(x)
+
 
 #############################################################################################3
 
@@ -34,37 +41,43 @@ print("3.Multiply")
 print("4.Divide")
 print("5.Power")
 print("6.Log")
-
-
-
+print("7.Square")
 
 ##############################################################################################3
 while True:
 
     choice = int(input("\nEnter your choice number: "))
 
-    if choice in range(1, 7):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+    if choice in range(1, 8):
+        if choice in range(1, 7):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
 
-        if choice == 1:
-            print(num1, "+", num2, "=", add(num1, num2))
+            if choice == 1:
+                print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == 2:
-            print(num1, "-", num2, "=", subtract(num1, num2))
+            elif choice == 2:
+                print(num1, "-", num2, "=", subtract(num1, num2))
 
-        elif choice == 3:
-            print(num1, "*", num2, "=", multiply(num1, num2))
-        elif choice == 4:
-            result = divide(num1, num2)
-            if result == ZeroDivisionError:
-                print("**** Zero Division Error! ****")
-            else:
-                print(num1, "/", num2, "=", result)
-        elif choice == 5:
-            print(num1, "**", num2, "=", power(num1, num2))
-        elif choice == 6:
-            print("The logarithm of ", num1, " with base ", num2, " is ",  log(num1, num2))
+            elif choice == 3:
+                print(num1, "*", num2, "=", multiply(num1, num2))
+
+            elif choice == 4:
+                result = divide(num1, num2)
+                if result == ZeroDivisionError:
+                    print("**** Zero Division Error! ****")
+                else:
+                    print(num1, "/", num2, "=", result)
+            elif choice == 5:
+                print(num1, "**", num2, "=", power(num1, num2))
+            elif choice == 6:
+                result = log(num1, num2)
+                print("The logarithm of ", num1, " with base ", num2, " is ", result)
+        else:
+            num = float(input("Enter the number: "))
+            if choice == 7:
+                result = square(num)
+                print("The square of ", num, " is ", result)
 
         next_calculation = input("Let's do next calculation? (y/n): ")
         if next_calculation == "n":
