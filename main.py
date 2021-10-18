9f0f4867530ad3621d75c67409c64b50a2355688
kmaroufi <kmaroufi76@gmail.com>
2021-10-18 20:11:49 +0330

    Cosine funcion

--- main.py
+++ main.py
@@ -38,6 +38,10 @@ def sin(x):
     return math.sin(x)  # in radian
 
 
+def cos(x):
+    return math.cos(x)  # in radian
+
+
 #############################################################################################3
 
 print("Select operation.")
@@ -49,13 +53,14 @@ print("5.Power")
 print("6.Log")
 print("7.Square")
 print("8.Sin")
+print("9.Cos")
 
 ##############################################################################################3
 while True:
 
     choice = int(input("\nEnter your choice number: "))
 
-    if choice in range(1, 9):
+    if choice in range(1, 10):
         if choice in range(1, 7):
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -90,6 +95,8 @@ while True:
                 print("The square of ", num, " is ", result)
             elif choice == 8:
                 print("Sin(", num, ") = ", sin(num))
+            elif choice == 9:
+                print("Cos(", num, ") = ", cos(num))
 
         next_calculation = input("Let's do next calculation? (y/n): ")
         if next_calculation == "n":
