164233ccf79a1ac78b24100000d67ca9bcb82664
vsaeide <vsaeide7798@gmail.com>
2021-10-18 11:51:15 +0330

    Log function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 def add(x, y):
     return x + y
 
@@ -20,6 +22,9 @@ def divide(x, y):
 def power(x, y):
     return x ** y
 
+def log(x, y):
+        return math.log(x, y)
+
 #############################################################################################3
 
 print("Select operation.")
@@ -28,6 +33,8 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 print("5.Power")
+print("6.Log")
+
 
 
 
@@ -36,7 +43,7 @@ while True:
 
     choice = int(input("\nEnter your choice number: "))
 
-    if choice in range(1, 5):
+    if choice in range(1, 7):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -56,6 +63,8 @@ while True:
                 print(num1, "/", num2, "=", result)
         elif choice == 5:
             print(num1, "**", num2, "=", power(num1, num2))
+        elif choice == 6:
+            print("The logarithm of ", num1, " with base ", num2, " is ",  log(num1, num2))
 
         next_calculation = input("Let's do next calculation? (y/n): ")
         if next_calculation == "n":
